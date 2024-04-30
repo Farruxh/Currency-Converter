884e2555c3f0cd099ede488b73597a901362bcf8
Farrukh <redd4940@gmail.com>
2024-05-01 01:50:17 +0500

    Correction of API

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const BASE_URL = 'https://openexchangerates.org/api/historical/';
-const today = new Date().toISOString().slice(0, 10);
-const app_id = '44f87c17c7d34bcda28a42a27723baaf';
-//const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-// the above url is from video
+const BASE_URL = "https://api.exchangerate-api.com/v4/latest";
 const dropdowns = document.querySelectorAll(".dropdown select");
 let btn = document.querySelector("button");
 let FromCurr = document.querySelector(".from select");
 let ToCurr = document.querySelector(".to select");
+let input = document.querySelector(".input");
+let message = document.querySelector(".msg");
 for(let select of dropdowns){
     for(currCode in countryList){
         let newOption = document.createElement("option");
@@ -18,10 +16,12 @@ for(let select of dropdowns){
          updateFlag(evt.target);
     });
 }
+document.addEventListener("load" , ()=>{
+
+})
 const updateFlag = (element) =>{
     let currCode = element.value;
-    let countryCode = countryList[currCode];
-    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png` ;
+    let newSrc = `https://flagsapi.com/${countryList[currCode]}/flat/64.png` ;
     let img = element.parentElement.querySelector("img"); 
     img.src = newSrc; 
 };
@@ -33,12 +33,10 @@ btn.addEventListener("click" , async (evty) =>{
         a = 1;
         amount.value=1;
     }
-    
-    // const URL = `${BASE_URL}/${FromCurr.value.toLowerCase()}/${ToCurr.value.toLowerCase()}.json`;
-    // the above url is from video
-    const URL = `${BASE_URL}${today}.json?base=${FromCurr.value.toLowerCase()}&symbols=${ToCurr.value.toLowerCase()}&app_id=${app_id}`;
-    
+    const URL = `${BASE_URL}/${FromCurr.value.toLowerCase()}`;
     let res = await fetch(URL);
-    let data = res.json();
-    console.log(data);
+    let data = await res.json();
+    let value =  data.rates[ToCurr.value]  * input.value;
+    // console.log(value);
+    message.innerHTML = `<div>${a} <b>${FromCurr.value}</b> = ${value} <b>${ToCurr.value}</b></div>`;
 })
