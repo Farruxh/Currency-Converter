5ec13a66aeb3edf62cdd9f3f348c8e3b38118762
Farrukh <farrukh.web2@gmail.com>
2024-05-03 15:35:24 +0500

    Add dark mode

--- script.js
+++ script.js
@@ -44,3 +44,52 @@ btn.addEventListener("click" , async (evty) =>{
     let value =  data.rates[ToCurr.value]  * input.value;
     message.innerHTML = `<div>${a} <b>${FromCurr.value}</b> = ${value} <b>${ToCurr.value}</b></div>`;
 })
+mode.addEventListener("click", () =>{
+    if(dark_mode === "white"){
+        body.classList.remove("body");
+        container.classList.remove("container");
+        btn.classList.remove("button");
+        heading2.classList.remove("heading");
+        message.classList.remove("msg");
+        para.classList.remove("para");
+        para_from.classList.remove("para_from");
+        para_to.classList.remove("para_to");
+    
+        body.classList.add("body1");
+        container.classList.add("container1");
+        btn.classList.add("button1");
+        heading2.classList.add("heading2");
+        message.classList.add("msg2");
+        para.classList.add("para2");
+        para_from.classList.add("para_from1");
+        para_to.classList.add("para_to1");
+        mode.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+        mode.classList.add("mode1");
+            
+        dark_mode = "black";
+    }
+    else if(dark_mode === "black"){
+        body.classList.remove("body1");
+        container.classList.remove("container1");
+        btn.classList.remove("button1");
+        heading2.classList.remove("heading2");
+        message.classList.remove("msg2");
+        para.classList.remove("para2");
+        para_from.classList.remove("para_from1");
+        para_to.classList.remove("para_to1");
+        mode.classList.remove("mode1");
+    
+        body.classList.add("body");
+        container.classList.add("container");
+        btn.classList.add("button");
+        heading2.classList.add("heading");
+        message.classList.add("msg");
+        para.classList.add("para");
+        para_from.classList.add("para_from");
+        para_to.classList.add("para_to");
+        mode.classList.add("mode");
+        mode.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+            
+        dark_mode = "white"; 
+    }
+});
